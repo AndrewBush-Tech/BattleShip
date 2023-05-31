70755de9de5572437d7b3918b26af3c9d8d75fd2
drewbush1990 <75611214+drewbush1990@users.noreply.github.com>
2023-05-31 15:04:29 -0700

    Update ShipGame.py

--- ShipGame.py
+++ ShipGame.py
@@ -1,137 +1,136 @@
 class ShipGame:
-    """A class to represent a Battleship game played by two players."""
-
     def __init__(self):
-        """Constructor for ShipGame class. Initializes the required data members."""
-
-        # Initialize game state
-        self._player1_turn = True
-        self._player2_turn = False
-        self._current_state = 'UNFINISHED'
-
-        # Initialize player data
+        self._player1_target_squares = []
+        self._player2_target_squares = []
         self._num_ships_remaining1 = 0
         self._num_ships_remaining2 = 0
         self._ship_list1 = {}
         self._ship_list2 = {}
-        self._player1_target_squares = []
-        self._player2_target_squares = []
+        self._current_state = 'UNFINISHED'
+        self._board_size = 10
+        self._board_player1 = self._create_board()
+        self._board_player2 = self._create_board()
 
-        # Initialize board data
-        self._board_player1 = self._initialize_board()
-        self._board_player2 = self._initialize_board()
-
-    def _initialize_board(self):
-        """Helper method to initialize the game board with coordinates."""
-
+    def _create_board(self):
         board = {}
-        letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-        for letter in letters:
-            for i in range(1, 11):
-                square = letter + str(i)
-                board[square] = [ord(letter) - ord('A'), i - 1]
+        for row in range(self._board_size):
+            for col in range(self._board_size):
+                square = f'{chr(row + 65)}{col + 1}'
+                board[square] = [row, col]
         return board
 
-    def place_ship(self, player_num, length_of_ship, start_square, ship_orientation):
-        """Places a ship on the game board for the specified player.
-        
-        Parameters:
-        - player_num: Represents the player's turn.
-        - length_of_ship: Length of the ship to be placed.
-        - start_square: The coordinate at which the player wishes to place the ship.
-        - ship_orientation: Orientation of the ship ('C' for column, 'R' for row).
-        
-        Returns:
-        - False: If ship does not fit into the required game board, overlaps with another ship, or invalid parameters.
-        - True: If the ship is successfully placed on the board.
-        """
-        if self._count_start != 0:
-            print("Already placed ship")
-            return False
+    def get_count(self, player_num):
+        if player_num == 'first':
+            return self._num_ships_remaining1
+        elif player_num == 'second':
+            return self._num_ships_remaining2
+        else:
+            return None
 
-        board = self._board_player1 if player_num == 'first' else self._board_player2
+    def set_count_start(self, count):
+        self._num_ships_remaining1 = count
+        self._num_ships_remaining2 = count
+
+    def get_count_start(self):
+        return self._num_ships_remaining1
+
+    def set_count(self, player_num, count):
+        if player_num == 'first':
+            self._num_ships_remaining1 = count
+        elif player_num == 'second':
+            self._num_ships_remaining2 = count
+
+    def place_ship(self, player_num, length_of_ship, start_square, ship_orientation):
+        if player_num == 'first':
+            board = self._board_player1
+            target_squares = self._player1_target_squares
+            ship_list = self._ship_list1
+            num_ships_remaining = self._num_ships_remaining1
+        elif player_num == 'second':
+            board = self._board_player2
+            target_squares = self._player2_target_squares
+            ship_list = self._ship_list2
+            num_ships_remaining = self._num_ships_remaining2
+        else:
+            return False
 
         if start_square not in board or board[start_square] == 'X':
             return False
 
         row, col = board[start_square]
-        max_row, max_col = max(board.values())
-
         if ship_orientation == 'C':
-            if row + length_of_ship - 1 > max_row:
+            if col + length_of_ship > self._board_size:
+                return False
+            for i in range(col, col + length_of_ship):
+                square = f'{chr(row + 65)}{i + 1}'
+                if board[square] == 'X':
+                    return False
+                target_squares.append(square)
+                ship_list[square] = num_ships_remaining
+        elif ship_orientation == 'R':
+            if row + length_of_ship > self._board_size:
                 return False
             for i in range(row, row + length_of_ship):
-                if board.get(chr(i + ord('A')) + str(col + 1)) == 'X':
+                square = f'{chr(i + 65)}{col + 1}'
+                if board[square] == 'X':
                     return False
-        elif ship_orientation == 'R':
-            if col + length_of_ship - 1 > max_col:
-                return False
-            for i in range(col,.
-                if board.get(chr(row + ord('A')) + str(i + 1)) == 'X':
-                    return False
+                target_squares.append(square)
+                ship_list[square] = num_ships_remaining
         else:
             return False
 
-        # Place the ship on the board
-        if ship_orientation == 'C':
-            for i in range(row, row + length_of_ship):
-                board[chr(i + ord('A')) + str(col + 1)] = 'X'
-        elif ship_orientation == 'R':
-            for i in range(col, col + length_of_ship):
-                board[chr(row + ord('A')) + str(i + 1)] = 'X'
-
-        # Update ship data for the player
-        if player_num == 'first':
-            self._num_ships_remaining1 += 1
-            self._ship_list1[start_square] = length_of_ship
-        else:
-            self._num_ships_remaining2 += 1
-            self._ship_list2[start_square] = length_of_ship
-
+        for square in target_squares:
+            board[square] = 'X'
+        
+        num_ships_remaining += 1
         return True
 
-    def _switch_turns(self):
-        """Helper method to switch turns between players."""
-        self._player1_turn = not self._player1_turn
-        self._player2_turn = not self._player2_turn
+    def get_current_state(self):
+        return self._current_state
+
+    def set_current_state(self, state):
+        self._current_state = state
 
     def fire_torpedo(self, player_num, target_square):
-        """Fires a torpedo at the specified target square for the player.
-        
-        Parameters:
-        - player_num: Represents the player's turn.
-        - target_square: The coordinate at which the player wishes to fire the torpedo.
-        
-        Returns:
-        - False: If the game is already finished, it's not the player's turn, or the target square has already been hit.
-        - True: If the torpedo successfully hits the target square.
-        """
-        if self._current_state != 'UNFINISHED' or (player_num == 'first' and not self._player1_turn) or (player_num == 'second' and not self._player2_turn):
+        if player_num == 'first':
+            board = self._board_player2
+            target_squares = self._player2_target_squares
+            ship_list = self._ship_list2
+            num_ships_remaining = self._num_ships_remaining2
+        elif player_num == 'second':
+            board = self._board_player1
+            target_squares = self._player1_target_squares
+            ship_list = self._ship_list1
+            num_ships_remaining = self._num_ships_remaining1
+        else:
             return False
 
-        board = self._board_player1 if player_num == 'first' else self._board_player2
-        target_board = self._board_player2 if player_num == 'first' else self._board_player1
-        target_squares = self._player1_target_squares if player_num == 'first' else self._player2_target_squares
-
-        if target_square not in target_board or target_square in target_squares:
+        if target_square not in board:
             return False
 
-        target_squares.append(target_square)
-
-        # Check if the torpedo hits a ship
-        if target_board[target_square] == 'X':
-            target_board[target_square] = 'H'
-            if player_num == 'first':
-                self._num_ships_remaining2 -= 1
-            else:
-                self._num_ships_remaining1 -= 1
+        if target_square in target_squares:
+            target_squares.remove(target_square)
+            if self.validate_ship_sunk(ship_list, num_ships_remaining):
+                num_ships_remaining -= 1
+                if num_ships_remaining == 0:
+                    self.set_current_state('WON')
+            return True
         else:
-            target_board[target_square] = 'M'
+            return False
 
-        # Check if the game is finished
-        if self._num_ships_remaining1 == 0 or self._num_ships_remaining2 == 0:
-            self._current_state = 'FINISHED'
+    def get_num_ships_remaining(self, player_num):
+        if player_num == 'first':
+            return self._num_ships_remaining1
+        elif player_num == 'second':
+            return self._num_ships_remaining2
         else:
-            self._switch_turns()
+            return None
 
-        return True
+    def validate_ship_sunk(self, ship_list, num_ships_remaining):
+        return all(ship == 0 for ship in ship_list.values())
+
+    def validate_winner(self):
+        if self._current_state == 'WON':
+            return 'first' if self._num_ships_remaining2 == 0 else 'second'
+        else:
+            return None
